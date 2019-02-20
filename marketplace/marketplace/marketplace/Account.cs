27ece6d48c384c545ce111de655991b380362e63
jrderoch@asu.edu <jrderoch@asu.edu>
2019-02-19 21:46:15 -0700

    server side

---
+++ marketplace/marketplace/marketplace/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace marketplace
+{
+    public class Account
+    {
+        public string UserID;
+        public string UserName;
+        public string UserPassword;
+        public string UserSecQuestion;
+        public string UserSecAnswer;
+        public string UserEmail;
+        public string UserShipAddress;
+        public string UserAbout;
+
+
+    }
+}
